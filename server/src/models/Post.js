42198619db0a32273f5de724b4fc79c58459d360
Your New Username <105604211+iaman8055@users.noreply.github.com>
2025-05-07 15:35:31 +0530

    update post schema to authorize the user to delete and update the post only by the user who created it

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   author: String,
   createdAt: { type: Date, default: Date.now },
 });
