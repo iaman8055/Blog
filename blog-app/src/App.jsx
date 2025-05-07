2a5e4ae8a92a7f3260a37e27c42924f09650eaed
Your New Username <105604211+iaman8055@users.noreply.github.com>
2025-05-07 14:03:59 +0530

    minor changes

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import CreatePost from "./pages/CreatePost";
 import EditPost from "./pages/EditPost";
 import Navbar from "./components/Navbar";
 import PostDetail from "./pages/PostDetails";
+import Signup from "./pages/SignUp";
 
 function App() {
   return (
